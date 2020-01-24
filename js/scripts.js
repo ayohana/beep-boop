5abacedd390a803c242437adbf245bee672ba62d
Adela <adela.yohana@gmail.com>
2020-01-24 09:53:13 -0800

    Add a function that returns a range of numbers from 0 to user inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
+// Back-end Logic
+var getNumberRange = function(input) {
+  var numbers = [];
+  for (var i = 0; i <= input; i++) {
+    numbers.push(i);
+  }
+  return numbers;
+}
+
+
+// Front-end Logic
 $(document).ready(function() {
   $("#beepBoopForm").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
-    console.log(userInput);
+
+    console.log(getNumberRange(userInput));
+    
   })
 });
