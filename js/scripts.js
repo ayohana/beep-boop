325750aee69037415a780b3214c354862d1abf1d
Adela <adela.yohana@gmail.com>
2020-01-25 12:07:45 -0800

    Refactor functions to check and replace 1, 2, 3

--- js/scripts.js
+++ js/scripts.js
@@ -15,71 +15,23 @@ var checkNumber = function(number) {
   return (isNaN(number) ? number = 3 : number);
 }
 
-var checkForOnes = function(number) {
+var checkAndReplace123 = function(rangeArray, inputName) {
   var ones = /[1]/gm;
-  if (number.match(ones)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-var checkForTwos = function(number) {
   var twos = /[2]/gm;
-  if (number.match(twos)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-var checkForThrees = function(number) {
   var threes = /[3]/gm;
-  if (number.match(threes)) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
-var replaceOnes = function(rangeArray) {
   for (var j = 0; j < rangeArray.length; j++) {
-    if (checkForOnes(rangeArray[j]) === true) {
+    if (rangeArray[j].match(threes)) {
+      rangeArray[j] = "I'm sorry, " + inputName + ". I'm afraid I can't do that.";
+    } else if (rangeArray[j].match(twos)) {
+      rangeArray[j] = "Boop!";
+    } else if (rangeArray[j].match(ones)) {
       rangeArray[j] = "Beep!";
     } else {
       continue;
     }
   }
   return rangeArray;
-}
-
-var replaceTwos = function(rangeArray) {
-  for (var j = 0; j < rangeArray.length; j++) {
-    if (checkForTwos(rangeArray[j]) === true) {
-      rangeArray[j] = "Boop!";
-    } else {
-      continue;
-    }
-  }
-  return rangeArray;
-}
-
-var replaceThrees = function(rangeArray, inputName) {
-  for (var j = 0; j < rangeArray.length; j++) {
-    if (checkForThrees(rangeArray[j]) === true) {
-      rangeArray[j] = "I'm sorry, " + inputName + ". I'm afraid I can't do that.";
-    } else {
-      continue;
-    }
-  }
-  return rangeArray;
-}
-
-var checkAndReplace123 = function(inputArray, inputName) {
-  replaceThrees(inputArray, inputName);
-  replaceTwos(inputArray);
-  replaceOnes(inputArray);
-  return inputArray;
 }
 
 var reverseOutput = function(outputArray) {
